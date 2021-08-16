e4501917a187f403ea5f5b9e5cb65400b9ff89ea
Jagadesh Ravuri <babu.ravuri@gmail.com>
2021-08-17 00:38:56 +0100

    implemented pdf verification test

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # web-automation-cypress
 This technical automation web test implementation using cypress.
+
+# Pre-requisites
+Install nodejs to get npm
+npm install cypress
+
+# Clone this repo and run below instructions in cloned directory
+ npm init
+ npm install --save-dev cypress
+ ./node_modules/.bin/cypress open
+
+# Run the tests by clicking below javascript files
+journey1.adviserguidepdf.js
+journey2.adviserregistrationvalidation.js
+
+# References
+ https://nodejs.org/en/
+ https://github.com/cypress-io/cypress-example-kitchensink
+ https://docs.cypress.io/guides/overview/why-cypress
+
+# Assumptions/Scope
+ chromeWebSecurity can be set to false in cypress.json for pdf tests but not considered.
+ Not placing the test document in git repo.
+ Clean-up tasks and installation scripts assumed out of scope.
--- journey1.adviserguidepdf.js
+++ journey1.adviserguidepdf.js
@@ -1,13 +1,17 @@
 /// <reference types="cypress" />
 
+let domainUrl = 'https://www.investcentre.co.uk'
+
 it('Adviser PDF guide display', () => {
   cy.viewport('macbook-13')
 
-  cy.visit('https://www.investcentre.co.uk')
+  cy.visit(domainUrl)
 
-  cy.get('input[name="adviser"]')
+  cy.get('.form-actions.js-form-wrapper.form-wrapper')
+    .children('.secondary.button.js-form-submit.form-submit')
     .contains('I am a financial professional')
     .click()
+
   cy.screenshot()
 
   cy.get('a[href="/retirement-investment-account"]')
@@ -18,18 +22,29 @@ it('Adviser PDF guide display', () => {
   cy.get('a[href="/platform/charges"]')
     .contains('Charges')
     .click({ force: true })
-
   cy.screenshot()
-
+  
   cy.get('a[href="/sites/default/files/AJBIC_charges_and_rates.pdf"]')
     .should('have.attr', 'target', 'blank')
+    .should('contain','CHARGES – SIPP, ISA OR GIA')
     .click()
-
-  //TODO: set "chromeWebSecurity": false in cypress.json file 
-  //per profile in lower environments and assert below for pdf
-  //cy.get('a[href="/sites/default/files/AJBIC_charges_and_rates.pdf"]')
-  //.should('remove.attr', 'target', 'blank')
-  //.click()
   
-})
+}),
 
+context('Download Pdf and ensure file exists', () => {
+  it('Download Pdf', () => {
+    cy.request({ url: domainUrl+'/sites/default/files/AJBIC_charges_and_rates.pdf', gzip: false, encodeBodyToBase64: true }).then(
+      (response) => {
+        const fileName = 'test-'+Date.now();
+        const filePath = 'cypress/downloads/' + fileName + '.pdf';
+
+        cy.writeFile(filePath, response.body, {
+          encoding: 'binary',
+          decodeContentFromBase64: true, 
+        });
+
+      cy.readFile(filePath)
+      }
+    );
+  });
+});
